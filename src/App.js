f2e913b26561ca31b40a3c4566bea835ede051a8
Kykuy <illidane2@gmail.com>
2021-01-04 16:04:34 +0300

    Revert "Started implementing async fetch id App.js"
    This reverts commit faecc5427c187091acb2e5da845c7c58640e231b.
    
    Revert unwanted changes(switched to wrong branch)

--- src/App.js
+++ src/App.js
@@ -4,70 +4,75 @@ import Searchbar from "./Searchbar.js";
 import Output from './Output.js';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);  
+  const [isLoading, setIsLoading] = useState(true);
+  const [all, setAll] = useState([]);  
   const [selectedDataType, setSelectedDataType] = useState('people');
   const [fetchUrl, setFetchUrl] = useState(`https://swapi.py4e.com/api/${selectedDataType}/`);
   const [searchInput, setSearchInput] = useState('');  
 
-  // useEffect(() => {
-  //   fetch(fetchUrl)
-  //     .then(response => response.json())           
-  //     .then(
-  //       result => {
-  //         setData(result);                 
-  //         setAll(result.results);                                  
-  //       },
-  //       error => {          
-  //         setError(error);
-  //         setIsLoading(false);
-  //       })              
-  //     .then(async () => {       
-  //       let numOfPages = Math.ceil(data.count / 10);
-  //       let pages = [];
-        
-  //       for (let num = 2; num <= numOfPages; num++) {
-  //         pages.push(num);
-  //       }   
-
-  //       console.log('numOfPages', numOfPages);
-  //       console.log('pages', pages);
-
-  //       let results = await Promise.all(pages.map(async item => {
-  //         const response = await fetch(`${fetchUrl}?page=${item}`);
-  //         const result = await response.json();
-  //         return result.results;
-  //       }));
-
-  //       setAll(prevAll => prevAll.concat(...results));
-  //     })
-  //     .then(() => setIsLoading(false));
-  // }, [fetchUrl, data.count]); 
-
   useEffect(() => {
-    getData(fetchUrl);
-  }, [fetchUrl]);
+    fetch(fetchUrl)
+      .then(response => response.json())           
+      .then(
+        result => {
+          setData(result);                 
+          setAll(result.results);                                  
+        },
+        error => {          
+          setError(error);
+          setIsLoading(false);
+        })              
+      .then(async () => {       
+        let numOfPages = Math.ceil(data.count / 10);
+        let pages = [];
+        
+        for (let num = 2; num <= numOfPages; num++) {
+          pages.push(num);
+        }   
 
-  async function getData(url) {    
-    let total = [];
+        console.log('numOfPages', numOfPages);
+        console.log('pages', pages);
 
-    let response = await fetch(url);
-    let data = await response.json();    
-    total = total.concat(data.results);    
-    
-    while(data.next) {      
-      if (data.next === null) {
-        break;
-      } else {
-        data = await fetch(data.next);
-        data = await data.json();        
-        total = total.concat(data.results);
-      }        
-    }
-    setData(total);
-    setIsLoading(false);    
-  }
+        let results = await Promise.all(pages.map(async item => {
+          const response = await fetch(`${fetchUrl}?page=${item}`);
+          const result = await response.json();
+          return result.results;
+        }));
+
+        setAll(prevAll => prevAll.concat(...results));
+      })
+      .then(() => setIsLoading(false));
+  }, [fetchUrl, data.count]); 
+
+  // async function getData() {
+  //   let page = 1;
+  //   let total = [];
+
+  //   let response = await fetch(`https://swapi.py4e.com/api/people/?page=${page}`);
+  //   let data = await response.json();
+  //   //console.log(data);
+  //   total = total.concat(data.results);
+  //   page++;
+  //   //console.log(total);
+  //   while(data.next) {
+  //       console.log(data.next);
+  //       if (data.next === null) {
+  //           break;
+  //       } else {
+  //           data = await fetch(`https://swapi.py4e.com/api/people/?page=${page}`);
+  //           data = await data.json();
+  //           console.log(data);
+  //           total = total.concat(data.results);    
+  //       }
+  //       page++;
+  //   }
+
+  //   console.log(total);
+  // }
+
+  // getData();
 
   //console.log(data);
   //console.log('all', all);
@@ -85,7 +90,7 @@ function App() {
       </header>
       <main>
         <Searchbar selected = {selectedDataType} setIsLoading = {setIsLoading} setFetchUrl = {setFetchUrl} setSelectedDataType = {setSelectedDataType} setSearchInput = {setSearchInput}/>
-        <Output data = {data} selected = {selectedDataType} searchInput = {searchInput} />
+        <Output data = {all} selected = {selectedDataType} searchInput = {searchInput} />
       </main>
       </>
     );
