b5ddfabf4b4d9b384fbf604246fb869d5daeed9d
Kykuy <illidane2@gmail.com>
2021-01-13 15:47:41 +0300

    Added previous and next page buttons

--- src/Pagination.js
+++ src/Pagination.js
@@ -2,48 +2,58 @@ import React, {Fragment} from 'react';
 
 function Pagination(props) {
   return (
-    <section>    
-    {Object.keys(props.pages).flatMap((page, index, array) => {
-      return page === '1' || page === props.pageSelected || page === array[array.length - 1] ?
-      
-      (
-        <button key = {`page #${page}`} onClick = {(event) => {
-          props.setPageSelected(page, 10);
-          }
-        }>{page}</button>
-      ) : Math.abs(page - props.pageSelected) === 1 ?
-
-        page - 1 > 1 && props.pageSelected - page === 1 ?
-
+    <section>
+    <button onClick = {(event) => {
+      if (props.pageSelected > 1) {
+        props.setPageSelected(prevPageSelected => prevPageSelected - 1);
+      }
+    }}>&lt;</button>    
+      {Object.keys(props.pages).flatMap((page, index, array) => {
+        return page === '1' || page === props.pageSelected || page === array[array.length - 1] ?
+        
         (
-          <Fragment key = {`page #${page}`}>
-          <span>...</span>
-          <button onClick = {(event) => {
-            props.setPageSelected(page, 10);
+          <button key = {`page #${page}`} onClick = {(event) => {
+            props.setPageSelected(page);
             }
           }>{page}</button>
-          </Fragment>
-        ) : array[array.length - 1] - page > 1 && props.pageSelected - page !== 1 ?
+        ) : Math.abs(page - props.pageSelected) === 1 ?
+
+          page - 1 > 1 && props.pageSelected - page === 1 ?
 
           (
-            <Fragment key = {`page #${page}`}>          
-            <button key = {`page #${page}`} onClick = {(event) => {
-              props.setPageSelected(page, 10);
-              }
-            }>{page}</button>
+            <Fragment key = {`page #${page}`}>
             <span>...</span>
-            </Fragment>
-          ) :
-
-          (
-            <button key = {`page #${page}`} onClick = {(event) => {
-              props.setPageSelected(page, 10);
+            <button onClick = {(event) => {
+              props.setPageSelected(page);
               }
             }>{page}</button>
+            </Fragment>
+          ) : array[array.length - 1] - page > 1 && props.pageSelected - page !== 1 ?
 
-      ) : [];
-    })}
-    </section>
+            (
+              <Fragment key = {`page #${page}`}>          
+              <button key = {`page #${page}`} onClick = {(event) => {
+                props.setPageSelected(page);
+                }
+              }>{page}</button>
+              <span>...</span>
+              </Fragment>
+            ) :
+
+            (
+              <button key = {`page #${page}`} onClick = {(event) => {
+                props.setPageSelected(page);
+                }
+              }>{page}</button>
+
+        ) : [];
+      })}
+      <button onClick = {(event) => {
+        if (props.pageSelected < Object.keys(props.pages)[Object.keys(props.pages).length - 1]) {
+          props.setPageSelected(prevPageSelected => prevPageSelected + 1);
+        }
+      }}>&gt;</button>
+    </section>    
   )
 }
 
