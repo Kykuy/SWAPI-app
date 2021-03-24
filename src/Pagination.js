c0f2718f00f7e1acc3a364a63872aea9b3e0ad01
Kykuy <illidane2@gmail.com>
2021-03-24 22:05:39 +0300

    Made pages buttons block width consistent

--- src/Pagination.js
+++ src/Pagination.js
@@ -7,58 +7,60 @@ function Pagination(props) {
 
   const pages = props.isSearching && props.data.length < 1 ? <>{resultsNumber}</> :
    <section className = 'pagination'>      
-    <button onClick = {(event) => {
-      if (props.pageSelected > 1) {
-        props.setPageSelected(prevPageSelected => prevPageSelected - 1);
-      }
-    }}>&lt;</button>
-
-    {Object.keys(props.pages).flatMap((page, index, array) => {
-      return parseInt(page, 10) === 1 || parseInt(page, 10) === props.pageSelected || parseInt(page, 10) === parseInt(array[array.length - 1], 10) ?
-      
-      (
-        <button disabled = {parseInt(page, 10) === props.pageSelected} key = {`page #${page}`} onClick = {(event) => {
-          props.setPageSelected(parseInt(page, 10));
+    <div className = 'pages'>
+      <button onClick = {(event) => {
+        if (props.pageSelected > 1) {
+          props.setPageSelected(prevPageSelected => prevPageSelected - 1);
         }
-        }>{page}</button>
-      ) : Math.abs(parseInt(page, 10) - props.pageSelected) === 1 ?
+      }}>&lt;</button>
 
-        parseInt(page, 10) - 1 > 1 && props.pageSelected - parseInt(page, 10) === 1 ?
-
+      {Object.keys(props.pages).flatMap((page, index, array) => {
+        return parseInt(page, 10) === 1 || parseInt(page, 10) === props.pageSelected || parseInt(page, 10) === parseInt(array[array.length - 1], 10) ?
+        
         (
-          <Fragment key = {`page #${page}`}>
-          <span>...</span>
-          <button onClick = {(event) => {
+          <button disabled = {parseInt(page, 10) === props.pageSelected} key = {`page #${page}`} onClick = {(event) => {
             props.setPageSelected(parseInt(page, 10));
           }
           }>{page}</button>
-          </Fragment>
-        ) : parseInt(array[array.length - 1], 10) - parseInt(page, 10) > 1 && props.pageSelected - parseInt(page, 10) !== 1 ?
+        ) : Math.abs(parseInt(page, 10) - props.pageSelected) === 1 ?
+
+          parseInt(page, 10) - 1 > 1 && props.pageSelected - parseInt(page, 10) === 1 ?
 
           (
-            <Fragment key = {`page #${page}`}>          
-            <button key = {`page #${page}`} onClick = {(event) => {
-              props.setPageSelected(parseInt(page, 10));
-            }
-            }>{page}</button>
+            <Fragment key = {`page #${page}`}>
             <span>...</span>
-            </Fragment>
-          ) :
-
-          (
-            <button key = {`page #${page}`} onClick = {(event) => {
+            <button onClick = {(event) => {
               props.setPageSelected(parseInt(page, 10));
             }
             }>{page}</button>
+            </Fragment>
+          ) : parseInt(array[array.length - 1], 10) - parseInt(page, 10) > 1 && props.pageSelected - parseInt(page, 10) !== 1 ?
 
-      ) : [];
-    })}
+            (
+              <Fragment key = {`page #${page}`}>          
+              <button key = {`page #${page}`} onClick = {(event) => {
+                props.setPageSelected(parseInt(page, 10));
+              }
+              }>{page}</button>
+              <span>...</span>
+              </Fragment>
+            ) :
 
-    <button onClick = {(event) => {
-      if (props.pageSelected < Object.keys(props.pages)[Object.keys(props.pages).length - 1]) {
-        props.setPageSelected(prevPageSelected => prevPageSelected + 1);
-      }
-    }}>&gt;</button>
+            (
+              <button key = {`page #${page}`} onClick = {(event) => {
+                props.setPageSelected(parseInt(page, 10));
+              }
+              }>{page}</button>
+
+        ) : [];
+      })}
+
+      <button onClick = {(event) => {
+        if (props.pageSelected < Object.keys(props.pages)[Object.keys(props.pages).length - 1]) {
+          props.setPageSelected(prevPageSelected => prevPageSelected + 1);
+        }
+      }}>&gt;</button>
+    </div>
 
     <label className = 'pageToJumpLabel' htmlFor = 'pageToJump'>Jump to page:</label>
     <input className = 'pageToJumpInput' id = 'pageToJump' name = 'pageToJump' type = 'number' min = '1' max = {Object.keys(props.pages).length} onChange = {(event) => {
